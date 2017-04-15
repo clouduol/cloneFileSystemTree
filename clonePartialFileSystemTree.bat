a2a7dad4d5c36c2d32fe15f417af6c6d14e765f4
learnMachining <guoyunlong16@mails.ucas.ac.cn>
2017-04-15 09:03:54 +0800

    remove bug marks

--- clonePartialFileSystemTree.bat
+++ clonePartialFileSystemTree.bat
@@ -1,16 +1,12 @@
 @echo off
 @rem chage page code to utf-8
-rem bug: sometimes, "for /f "delima=" %%i in['dir ...']" echo %%i is different from dir ...
-rem		for example, directory "グレイテスト・マキシム" will be change to "グレイテスト?マキシム"
-rem fix: use chcp 65001, change code page to utf-8
 chcp 65001
 
 echo begin clone
 date /t
 time /t
-@rem all directory paths are full and end without "\" && all driver marks are lower letters
 set "drivers=a b c d e f g h i j k l m n o p q r s t u v w x y z"
-@rem user dependent, set firstly
+@rem user dependent batch file parameters, set firstly
 set "cloneRootDirectory=d:\cloneFileSystemTree"
 set "cloneRootDirectory2=c:\cloneFileSystemTree"
 set "maxDepth=4"
@@ -46,28 +42,20 @@ goto:eof
 @rem @param %3 left depth
 :clone 
 setlocal
-	@rem process input path
-	rem bug: delete "" around firstParam=%1 to avoid %1 contians &, like "this & that"
+	@rem process parameters
 	set "firstParam=%~1"
 	set "secondParam=%~2"
 	set "leftDepth=%3"
 	set /A "leftDepth-=1"
 
-	rem bug: using "" in set statement, avoid errors when %1 or %2 contains bracket
 	if "%firstParam%"=="root" (
 		set "currentDirectory=%secondParam%"
 	) else (
 		set "currentDirectory=%firstParam%\%secondParam%"
 	)
-
-	@rem use \\?\ magic prefix before driver mark c,d,etc to avoid long path more than 260 bytes
-	@rem to preserve consistency, use \\?\ in specific command
-	@rem echo>FILE is useful, md DIRECTORY is not useful
-	@rem for insurance reasons, chagne Registry[only for windows 10]
+	echo "%currentDirectory%"
 	set "cloneCurrentDirectory=%cloneRootDirectory%\%currentDirectory::=%"
 	set "cloneCurrentDirectory2=%cloneRootDirectory2%\%currentDirectory::=%"
-
-	echo "%currentDirectory%"
 
 	@rem filter
 	if "%cloneRootDirectory%"=="%currentDirectory%" (
@@ -97,18 +85,14 @@ setlocal
 	set "cloneDFile=%cloneCurrentDirectory%\%secondParam::=%_directories.txt"
 	set "cloneDFile2=%cloneCurrentDirectory2%\%secondParam::=%_directories.txt"
 
-	@rem clone file names
-	@rem avoid output "File Not Found" error
-	rem bug: use \\?\ before directory name, OR sometimes "..." tail cause error
+	@rem record file names
 	dir /A-D-S "\\?\%currentDirectory%\" > nul 2>&1
 	if %errorlevel% equ 0 (
 		dir /A-D-S "\\?\%currentDirectory%\" > "\\?\%cloneFFile%"
 		dir /A-D-S "\\?\%currentDirectory%\" > "\\?\%cloneFFile2%"
 	)
 
-	@rem recursively clone, nested clone directory names
-	rem bug: using "" in brackts, avoid errors when %currentDirectory% contains bracket
-	rem bug: using "" around parameters, avoid directory that contains bracket ; actually, all directories should be around ""
+	@rem recursively clone, nestedly record directory names
 	for /f "delims=" %%i in ('dir /AD-S /B "\\?\%currentDirectory%\"') do (
 		if not exist "\\?\%cloneDFile%" (
 			dir /AD-S "\\?\%currentDirectory%\" > "\\?\%cloneDFile%"
@@ -117,7 +101,7 @@ setlocal
 			dir /AD-S "\\?\%currentDirectory%\" > "\\?\%cloneDFile2%"
 		)
 
-		@rem bug: four % pass one, to balance between % and ! is hard, so tree /f directly
+		@rem process directory name that contains percent sign
 		echo "%%i" | findstr /C:"%%" > nul
 		if errorlevel 1 (
 			call :clone "%currentDirectory%","%%i",%leftDepth%
@@ -126,6 +110,5 @@ setlocal
 			tree /F "%currentDirectory%\%%i" > "%cloneCurrentDirectory2%\%secondParam::=%_%%i_tree.txt"
 		)
 	) 
-
 endlocal
 goto:eof
